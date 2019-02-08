d0dbd28a0d542ef60b4457960bd24099bab7cfb5
Poorna Chandra Tejasvi <pct960@gmail.com>
2019-02-08 18:48:27 +0530

    WIP: Adding quick install

---
+++ setup/vertx/vertx_quick_install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd iudx-api-server 
+
+cp target/iudx-api-server-0.0.1-SNAPSHOT-fat.jar .
+
+tmux new-session -d -s vertx 'java -jar iudx-api-server-0.0.1-SNAPSHOT-fat.jar -d64 -Xms512m -Xmx4g'
