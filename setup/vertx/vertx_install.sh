85b018860c5e2222b96c6edc493feb88641fa621
Poorna Chandra Tejasvi <pct960@gmail.com>
2019-02-08 13:47:03 +0530

    Updated install file

--- setup/vertx/vertx_install.sh
+++ setup/vertx/vertx_install.sh
@@ -6,4 +6,4 @@ mvn package
 
 cp target/iudx-api-server-0.0.1-SNAPSHOT-fat.jar .
 
-tmux new-session -d -s vertx 'java -jar iudx-api-server-0.0.1-SNAPSHOT-fat.jar'
+tmux new-session -d -s vertx 'java -jar iudx-api-server-0.0.1-SNAPSHOT-fat.jar -d64 -Xms512m -Xmx4g'
