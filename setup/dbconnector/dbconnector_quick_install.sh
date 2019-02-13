49151fd5ba3788ffcd80652b8c44c2679c154929
Swaminathan Vasanth Rajaraman <swaminathanvasanth.r@gmail.com>
2019-02-13 17:37:15 +0530

    Added dbconnector container and installation scripts

---
+++ setup/dbconnector/dbconnector_quick_install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd iudx-dbconnector
+
+cp target/iudx-dbconnector-0.0.1-SNAPSHOT-jar-with-dependencies.jar .
+
+tmux new-session -d -s dbconnector 'java -jar iudx-dbconnector-0.0.1-SNAPSHOT-jar-with-dependencies.jar -d64 -Xms512m -Xmx4g'
