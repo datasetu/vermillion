389d2fd9a8993556ee899a17a3b316a504f90627
Poorna Chandra Tejasvi <pct960@gmail.com>
2019-05-26 14:30:16 +0530

    WIP: Swarm mode deployment

--- swarm/gen-compose.py
+++ swarm/gen-compose.py
@@ -2,6 +2,7 @@
 
 import json
 import sys
+import argparse
 
 base_dict = {}
 
@@ -24,8 +25,33 @@ class Node:
 
 if __name__ == "__main__":
 
-    workers, proxies = sys.argv[1].split(":")
-    
+    parser = argparse.ArgumentParser(description="Specifications to generate compose file")
+
+    parser.add_argument(
+        "-w",
+        "--workers",
+        action="store",
+        dest="workers",
+        type=int,
+        help="No. of workers to deploy in the swarm cluster",
+        )
+
+    parser.add_argument(
+        "-p",
+        "--proxies",
+        action='store',
+        dest="proxies",
+        type=int,
+        help="No. of HTTP proxies to deploy in the swarm cluster",
+        )
+
+    args = parser.parse_args()
+
+    workers = int(args.workers)
+    proxies = int(args.proxies)
+
+    rabbit_list = ["rabbit" + str(i) for i in range(1,workers+1)]
+
     node_list = [Node(node).populate_node_name() for node in range(0, int(workers))]
 
     base_dict = json.loads(open("compose-template.json", "r").read())
@@ -39,6 +65,9 @@ if __name__ == "__main__":
         counter = counter + 1
 
     base_dict["services"]["vertx"]["deploy"]["replicas"] = int(proxies)
+    #base_dict["services"]["vertx"]["depends_on"] = rabbit_list + ["postgres"]
+    #base_dict["services"]["dbconnector"]["depends_on"] = rabbit_list + ["elk"]
+    #base_dict["services"]["unbind-daemon"]["depends_on"] = rabbit_list + ["postgres"]
 
     f = open("docker-compose.json", "w")
     f.write(json.dumps(base_dict))
