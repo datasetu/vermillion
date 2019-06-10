ef3558a10508ac850f8802a32a2d4fe61360151e
Poorna Chandra Tejasvi <pct960@gmail.com>
2019-06-10 15:09:54 +0530

    WIP: Cluster mode

---
+++ images/rabbitmq-cluster/docker-entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/ash
+
+set -e
+
+mkdir -p /var/lib/rabbitmq/
+cookie_file='/usr/lib/rabbitmq/.erlang.cookie'
+echo $ERLANG_COOKIE > $cookie_file
+chmod 600 "$cookie_file"
+chown rabbitmq "$cookie_file"
+
+hostname=`hostname`
+
+if [ $hostname == "rabbit" -o $hostname == "rabbit1" ]
+then
+    rabbitmq-server
+else
+    rabbitmq-server -detached
+    rabbitmqctl stop_app
+    rabbitmqctl join_cluster rabbit@rabbit1
+    rabbitmqctl stop
+    sleep 2s
+    rabbitmq-server
+fi
+
+
