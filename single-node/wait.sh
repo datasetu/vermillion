1c2f9622ec4a27c244189ffcb781fb4464f3bd4f
Poorna Chandra Tejasvi <pct960@gmail.com>
2020-11-17 16:42:00 +0530

    Using symlinks instead of mv

--- single-node/wait.sh
+++ single-node/wait.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #Wait for the auth server to come up
-until $(curl -k -XPOST --output /dev/null --silent https://localhost:8443/certificate-info --cert ../datasetu-ca/provider/provider.pem --key ../datasetu-ca/provider/provider.key.pem); do
+until $(curl -k -XPOST --output /dev/null --silent https://localhost); do
     printf '.'
     sleep 1
 done
