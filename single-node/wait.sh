0cf21cb227f4477740c0efcf990071e167468640
Poorna Chandra Tejasvi <pct960@gmail.com>
2020-11-17 15:43:57 +0530

    Fix

--- single-node/wait.sh
+++ single-node/wait.sh
@@ -2,5 +2,8 @@
 
 #Wait for the auth server to come up
 until $(curl -k -XPOST --output /dev/null --silent https://localhost:8443/certificate-info --cert ../datasetu-ca/provider/provider.pem --key ../datasetu-ca/provider/provider.key.pem); do
+    printf '.'
     sleep 1
 done
+
+echo "ready"
