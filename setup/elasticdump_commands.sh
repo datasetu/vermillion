27780a3a561dec61ba79693a8cb4506d92fb69dd
Poorna Chandra Tejasvi <pct960@gmail.com>
2020-11-24 10:44:35 +0530

    Code cleanup

--- setup/elasticdump_commands.sh
+++ setup/elasticdump_commands.sh
@@ -2,12 +2,12 @@
 
 #TODO: Make sure paths are correct when running this file
 
-docker run --rm -ti --network single-node_vermillion-net -v ${PWD}/data:/tmp elasticdump/elasticsearch-dump \
+docker run --rm -ti --network setup_vermillion-net -v ${PWD}/data:/tmp elasticdump/elasticsearch-dump \
     --input=http://elasticsearch:9200/archive \
     --output=http://elasticsearch:9200/archive_sampled \
     --type=mapping
 
-docker run --rm -ti --network single-node_vermillion-net -v ${PWD}/data:/tmp elasticdump/elasticsearch-dump \
+docker run --rm -ti --network setup_vermillion-net -v ${PWD}/data:/tmp elasticdump/elasticsearch-dump \
     --input=http://elasticsearch:9200/archive \
     --output=http://elasticsearch:9200/archive_sampled \
     --size=10000 \
@@ -15,7 +15,7 @@ docker run --rm -ti --network single-node_vermillion-net -v ${PWD}/data:/tmp ela
     --searchBody='{"query":{"bool":{"filter":[{"term":{"category.keyword":"varanasi-swm-vehicles"}}]}}}' \
     --type=data
 
-docker run --rm -ti --network single-node_vermillion-net -v ${PWD}/data:/tmp elasticdump/elasticsearch-dump \
+docker run --rm -ti --network setup_vermillion-net -v ${PWD}/data:/tmp elasticdump/elasticsearch-dump \
     --input=http://elasticsearch:9200/archive \
     --output=http://elasticsearch:9200/archive_sampled \
     --size=10000 \
@@ -23,14 +23,14 @@ docker run --rm -ti --network single-node_vermillion-net -v ${PWD}/data:/tmp ela
     --searchBody='{"query":{"bool":{"filter":[{"term":{"category.keyword":"varanasi-swm-workers"}}]}}}' \
     --type=data
 
-docker run --rm -ti --network single-node_vermillion-net -v ${PWD}/data:/tmp elasticdump/elasticsearch-dump \
+docker run --rm -ti --network setup_vermillion-net -v ${PWD}/data:/tmp elasticdump/elasticsearch-dump \
     --input=http://elasticsearch:9200/archive \
     --output=http://elasticsearch:9200/archive_sampled \
     --limit=5000 \
     --searchBody='{"query":{"bool":{"filter":[{"term":{"category.keyword":"varanasi-aqm"}}]}}}' \
     --type=data
 
-docker run --rm -ti --network single-node_vermillion-net -v ${PWD}/data:/tmp elasticdump/elasticsearch-dump \
+docker run --rm -ti --network setup_vermillion-net -v ${PWD}/data:/tmp elasticdump/elasticsearch-dump \
     --input=http://elasticsearch:9200/archive \
     --output=http://elasticsearch:9200/archive_sampled \
     --limit=5000 \
