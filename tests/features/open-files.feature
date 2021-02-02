ffb6df1521e1a5f063fa27d695d555adbdc59559
vasi <lvasanth096@gmail.com>
2021-02-02 09:23:42 +0530

    Added test cases for open-files

---
+++ tests/features/open-files.feature
@@ -0,0 +1,57 @@
+Feature: Vermillion is able to handle static files
+
+
+        
+ 	Scenario: Consumer requests for tokens
+                Given Vermillion is running
+                When The consumer requests token 
+		Then The response status should be 201 
+                                            
+	Scenario: Consumer requests for tokens
+                Given Vermillion is running
+                When The consumer requests with invalid payload id
+                Then The response status should be 500
+
+		
+        Scenario: Consumer requests for tokens
+                Given Vermillion is running
+                When The consumer requests with empty payload id
+                Then The response status should be 500
+		
+	
+	Scenario: Consumer requests for tokens
+                Given Vermillion is running
+                When The consumer requests with invalid payload token
+		Then The response status should be 403
+
+ 	Scenario: Consumer requests for tokens
+                Given Vermillion is running
+                When The consumer requests with empty payload token
+                Then The response status should be 403
+		
+	 Scenario: Consumer requests for tokens
+                Given Vermillion is running
+                When The consumer requests with invalid payload
+                Then The response status should be 500
+
+	 Scenario: Consumer requests for tokens
+                Given Vermillion is running
+                When The consumer requests with empty payload
+                Then The response status should be 400
+
+	Scenario: Consumer requests for tokens
+                Given Vermillion is running
+                When The consumer requests by removing file form parameter 
+                Then The response status should be 400
+
+        Scenario: Consumer requests for tokens
+                Given Vermillion is running
+                When The consumer requests by removing metadata form parameter  
+                Then The response status should be 201
+
+	 Scenario: Consumer requests for tokens
+                Given Vermillion is running
+                When The consumer requests with empty form parameter  
+                Then The response status should be 400
+
+
--- tests/features/steps/open-files.py
+++ tests/features/steps/open-files.py
@@ -6,8 +6,259 @@ from requests.packages.urllib3.exceptions import InsecureRequestWarning
 
 VERMILLION_URL = 'https://localhost'
 SEARCH_ENDPOINT = '/search'
+PUBLISH_ENDPOINT= '/publish'
+
+
+FilePath1='../datasetu-ca/consumer/sample.txt'
+FilePath2='../datasetu-ca/consumer/meta.json'
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 #XXX Open-files tests need definition here
+
+@when('The consumer requests token')
+def step_imp(context):
+    payload= (
+           ("id","rbccps.org/e096b3abef24b99383d9bd28e9b8c89cfd50be0b/example.com/test-category/test-resource.public" 
+            ),
+    ('token', 'auth.local/consumer@iisc.ac.in/291f28ecbc779160d32eb49c7081f952'),
+
+)
+    files = {
+    'file': ('sample.txt', open(FilePath1, 'rb')),
+    'metadata': ('meta.json', open(FilePath2, 'rb')),
+    }
+
+
+    
+    r=requests.post(url=VERMILLION_URL + PUBLISH_ENDPOINT,
+                    data=payload,
+                    files=files,   
+                    verify=False)
+
+    context.response= r
+    context.status_code=r.status_code
+    print(context.status_code,context.response)
+
+
+@when('The consumer requests with invalid payload id')
+def step_imp(context):
+    payload= (
+           ("id","kjghiushoi soishgoishogi hsoh oioi soighoishg"
+            ),
+    ('token', 'auth.local/consumer@iisc.ac.in/291f28ecbc779160d32eb49c7081f952'),
+
+)
+    files = {
+    'file': ('sample.txt', open(FilePath1, 'rb')),
+    'metadata': ('meta.json', open(FilePath2, 'rb')),
+    }
+
+
+
+    r=requests.post(url=VERMILLION_URL + PUBLISH_ENDPOINT,
+                    data=payload,
+                    files=files,
+                    verify=False)
+
+    context.response= r
+    context.status_code=r.status_code
+    print(context.status_code,context.response)
+
+
+
+@when('The consumer requests with empty payload id')
+def step_imp(context):
+    payload= (
+           ("id",""
+            ),
+    ('token', 'auth.local/consumer@iisc.ac.in/291f28ecbc779160d32eb49c7081f952'),
+
+)
+    files = {
+    'file': ('sample.txt', open(FilePath1, 'rb')),
+    'metadata': ('meta.json', open(FilePath2, 'rb')),
+    }
+
+
+
+    r=requests.post(url=VERMILLION_URL + PUBLISH_ENDPOINT,
+                    data=payload,
+                    files=files,
+                    verify=False)
+
+    context.response= r
+    context.status_code=r.status_code
+    print(context.status_code,context.response)
+
+@when('The consumer requests with invalid payload token')
+def step_imp(context):
+    payload= (
+           ("id","rbccps.org/e096b3abef24b99383d9bd28e9b8c89cfd50be0b/example.com/test-category/test-resource.public"
+            ),
+    ('token', 'ufsghushfus shfguhsoig sohg ohs gsh gshgouusg hosi9873598735895389'),
+
+)
+    files = {
+    'file': ('sample.txt', open(FilePath1, 'rb')),
+    'metadata': ('meta.json', open(FilePath2, 'rb')),
+    }
+
+
+
+    r=requests.post(url=VERMILLION_URL + PUBLISH_ENDPOINT,
+                    data=payload,
+                    files=files,
+                    verify=False)
+
+    context.response= r
+    context.status_code=r.status_code
+    print(context.status_code,context.response)
+
+@when('The consumer requests with empty payload token')
+def step_imp(context):
+    payload= (
+           ("id","rbccps.org/e096b3abef24b99383d9bd28e9b8c89cfd50be0b/example.com/test-category/test-resource.public"
+            ),
+    ('token', ''),
+
+)
+    files = {
+    'file': ('sample.txt', open(FilePath1, 'rb')),
+    'metadata': ('meta.json', open(FilePath2, 'rb')),
+    }
+
+
+
+    r=requests.post(url=VERMILLION_URL + PUBLISH_ENDPOINT,
+                    data=payload,
+                    files=files,
+                    verify=False)
+
+    context.response= r
+    context.status_code=r.status_code
+    print(context.status_code,context.response)
+
+@when('The consumer requests with invalid payload')
+def step_imp(context):
+    payload= (
+           ("id","kjfghkushf shgshgkhsgk sghlslhg lls"
+            ),
+    ('token', 'kjhfg jkhskghksj hk skfgsk gf ksf '),
+
+)
+    files = {
+    'file': ('sample.txt', open(FilePath1, 'rb')),
+    'metadata': ('meta.json', open(FilePath2, 'rb')),
+    }
+
+
+
+    r=requests.post(url=VERMILLION_URL + PUBLISH_ENDPOINT,
+                    data=payload,
+                    files=files,
+                    verify=False)
+
+    context.response= r
+    context.status_code=r.status_code
+    print(context.status_code,context.response)
+
+@when('The consumer requests with empty payload')
+def step_imp(context):
+    payload= (
+           #("id","rbccps.org/e096b3abef24b99383d9bd28e9b8c89cfd50be0b/example.com/test-category/test-resource.public"
+            #),
+    #('token', ''),
+
+)
+    files = {
+    'file': ('sample.txt', open(FilePath1, 'rb')),
+    'metadata': ('meta.json', open(FilePath2, 'rb')),
+    }
+
+
+
+    r=requests.post(url=VERMILLION_URL + PUBLISH_ENDPOINT,
+                    data=payload,
+                    files=files,
+                    verify=False)
+
+    context.response= r
+    context.status_code=r.status_code
+    print(context.status_code,context.response)
+
+@when('The consumer requests by removing file form parameter')
+def step_imp(context):
+    payload= (
+           ("id","rbccps.org/e096b3abef24b99383d9bd28e9b8c89cfd50be0b/example.com/test-category/test-resource.public"
+            ),
+    ('token', 'auth.local/consumer@iisc.ac.in/291f28ecbc779160d32eb49c7081f952'),
+
+)
+    files = {
+    #'file': ('sample.txt', open(FilePath1, 'rb')),
+    'metadata': ('meta.json', open(FilePath2, 'rb')),
+    }
+
+
+
+    r=requests.post(url=VERMILLION_URL + PUBLISH_ENDPOINT,
+                    data=payload,
+                    files=files,
+                    verify=False)
+
+    context.response= r
+    context.status_code=r.status_code
+    print(context.status_code,context.response)
+
+@when('The consumer requests by removing metadata form parameter')
+def step_imp(context):
+    payload= (
+           ("id","rbccps.org/e096b3abef24b99383d9bd28e9b8c89cfd50be0b/example.com/test-category/test-resource.public"
+            ),
+    ('token', 'auth.local/consumer@iisc.ac.in/291f28ecbc779160d32eb49c7081f952'),
+
+)
+    files = {
+    'file': ('sample.txt', open(FilePath1, 'rb')),
+    #'metadata': ('meta.json', open(FilePath2, 'rb')),
+    }
+
+
+
+    r=requests.post(url=VERMILLION_URL + PUBLISH_ENDPOINT,
+                    data=payload,
+                    files=files,
+                    verify=False)
+
+    context.response= r
+    context.status_code=r.status_code
+    print(context.status_code,context.response)
+
+
+@when('The consumer requests with empty form parameter')
+def step_imp(context):
+    payload= (
+           ("id","rbccps.org/e096b3abef24b99383d9bd28e9b8c89cfd50be0b/example.com/test-category/test-resource.public"
+            ),
+    ('token', 'auth.local/consumer@iisc.ac.in/291f28ecbc779160d32eb49c7081f952'),
+
+)
+    files = {
+    #'file': ('sample.txt', open(FilePath1, 'rb')),
+    #'metadata': ('meta.json', open(FilePath2, 'rb')),
+    }
+
+
+
+    r=requests.post(url=VERMILLION_URL + PUBLISH_ENDPOINT,
+                    data=payload,
+                    files=files,
+                    verify=False)
+
+    context.response= r
+    context.status_code=r.status_code
+    print(context.status_code,context.response)
+
+                    
