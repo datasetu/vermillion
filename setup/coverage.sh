9f0adce87ad3894cba2e394e9dd4a95b7eeba40d
Vasanth Lakshminarayanan <vasanth@Vasanths-MacBook-Pro.local>
2021-03-30 12:12:08 +0530

    Update codacy

--- setup/coverage.sh
+++ setup/coverage.sh
@@ -14,7 +14,11 @@ mvn jacoco:report
 sleep 10
 echo statement6
 wget https://github.com/codacy/codacy-coverage-reporter/releases/tag/11.13.2/codacy-coverage-reporter-assembly.jar
-export CODACY_PROJECT_TOKEN=${{ secrets.CODACY_TOKEN }}
+steps:
+  - shell: bash
+    env:
+      CODACY_SECRET: ${{ secrets.CODACY_TOKEN }}
+export CODACY_PROJECT_TOKEN="$CODACY_SECRET"
 bash <(curl -Ls https://coverage.codacy.com/get.sh) report -r target/site/jacoco/jacoco.xml
 
 
