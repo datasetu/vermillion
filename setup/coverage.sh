d5224aa8550de0d802e085c1b52b3b0ac83f3040
Poorna Chandra Tejasvi <pct960@gmail.com>
2021-04-07 08:50:26 +0530

    Updated coverage script

--- setup/coverage.sh
+++ setup/coverage.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
 
+set -ex
 
 cd ../tests && sudo behave
-echo statement3
-PID=`docker exec vertx ps -aux | grep java | awk '{print $2}'`
+PID=$(docker exec vertx ps -aux | grep java | awk '{print $2}')
 echo "killing $PID"
 docker exec vertx kill -15 $PID
 sleep 15
-echo statement4
 cd ../api-server
-echo statement5
 mvn jacoco:report
 sleep 10
-echo statement6
-cp target/site/jacoco/jacoco.xml ../.github/workflows
-#export CODACY_PROJECT_TOKEN=$secrets.CODACY_KEY
-#bash <(curl -Ls https://coverage.codacy.com/get.sh) report -r target/site/jacoco/jacoco.xml
+echo "Done"
 
 
 
