3d40789423697a6ec1020902d9f1dba7424dc81a
Poorna Chandra Tejasvi <pct960@gmail.com>
2021-04-07 08:38:29 +0530

    Updated cleanup file

--- setup/cleanup.sh
+++ setup/cleanup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-cd provider
-rm -r ./*
+set -ex
+
+cd provider && rm -rf ./*
 cd ../../api-server
 rm org.jacoco.agent-0.8.6-runtime.jar
-cd webroot/consumer
-rm -r ./*
+cd webroot/consumer && rm -rf ./*
 cd ../../../tests
 rm test-resource.public
