fb148c4c8ff167c3e1a743ba053207991d3f487a
Poorna Chandra Tejasvi <pct960@gmail.com>
2021-04-08 12:10:26 +0530

    Automate switch to test env

--- docker-images/java/docker-entrypoint.sh
+++ docker-images/java/docker-entrypoint.sh
@@ -4,28 +4,8 @@ set -e
 
 [ -z "$PROJECT_DIR" ] && echo "PROJECT_DIR is not set" && exit 1
 [ -z "$JAR_NAME" ] && echo "JAR_NAME is not set" && exit 1
-#[ -z "$JAVA_OPTS" ] && echo "JAR_OPTS is not set" && exit 1
-if [ "$ENV" == "test" ];
-then
-  if [ "$RUN_ONLY" == "true" ];
 
-    then
-      cd $PROJECT_DIR
-      if [ -e $JAR_NAME ]
-      then
-	      rm $JAR_NAME
-      fi
-      cp target/$JAR_NAME .
-      java $JACOCO_OPTS $JAVA_OPTS -jar $JAR_NAME
-  else
-    cd $PROJECT_DIR
-    mvn $MVN_OPTS clean package
-    cp target/$JAR_NAME .
-    java $JACOCO_OPTS $JAVA_OPTS -jar $JAR_NAME
-fi
-else
 if [ "$RUN_ONLY" == "true" ];
-
 then
     cd $PROJECT_DIR
     if [ -e $JAR_NAME ]
@@ -33,12 +13,24 @@ then
 	rm $JAR_NAME
     fi
     cp target/$JAR_NAME .
-    java $JAVA_OPTS -jar $JAR_NAME
+
+    if [ $VERMILLION_ENV == "test" ];
+    then
+	JAVA_OPTS="$COVERAGE_OPTS $JAVA_OPTS"
+    fi
+
+    echo "COMMAND: java $JAVA_OPTS -jar $JAR_NAME" 
+    java $JAVA_OPTS -jar $JAR_NAME 
 else
     cd $PROJECT_DIR
     mvn $MVN_OPTS clean package
     cp target/$JAR_NAME .
-    java $JAVA_OPTS -jar $JAR_NAME
+
+    if [ $VERMILLION_ENV == "test" ];
+    then
+	JAVA_OPTS="$COVERAGE_OPTS $JAVA_OPTS"
+    fi
+
+    echo "COMMAND: java $JAVA_OPTS -jar $JAR_NAME"
+    java $JAVA_OPTS -jar $JAR_NAME 
 fi
-fi
-ENV JAVA_TOOL_OPTIONS
